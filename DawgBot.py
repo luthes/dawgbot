c52cd89ee162851ff5f466223d0251a742516bf0
luthes <steve.luther@gmail.com>
2017-05-13 20:25:44 -0700

    Added Feature and Bug commands

--- DawgBot.py
+++ DawgBot.py
@@ -1,20 +1,24 @@
 import sys, time
-from daemon import Daemon
 import asyncio
 import requests
 from bs4 import BeautifulSoup as BS
 import discord
 from discord.ext import commands
 
-description = '''
+# Global Variables
+
+DESCRIPTION= '''
 Thanks for checking out DawgBot! If you need any help with commands, type !help.
-For help with a specific command, type !command help.
+For help with a specific command, type !command help. 
+
+Send any feature requests to zer0, or write them yourself and submit a pull 
+request on GitHub (use !source).
 '''
 
 # Get from file
 with open('DawgBot.key', 'r') as file:
     key=file.read().replace('\n','')
-bot = commands.Bot(command_prefix='!', description=description)
+bot = commands.Bot(command_prefix='!', description=DESCRIPTION)
 client = discord.Client()
     
 @bot.command()
@@ -24,7 +28,49 @@ async def hello(member : discord.Member):
 
     Syntax: !hello zer0
     """
+    for i in args:
+         member += " ".join(args) 
+    print(member)
     await bot.say('Hello, {0.name}, welcome!'.format(member))
+
+@bot.command()
+async def feature(*args):
+    """ Feature Requests are held server side. Thanks for the input! 
+        File structure: (NUMBER)(DATE)(TIME) - Feature#: (string) \n
+
+        Syntax !feature (string)
+    """ 
+    featureCount = 0
+    print("Lines in Feature File: " + str(featureCount))
+    featureFile = open('featureRequests.txt', 'r+')
+    for i in featureFile.readlines():
+        print("feature lines:" + i)
+        featureCount += 1
+    for i in args:
+        print("Arguments: " + i)
+        featureArgs = " ".join(args)
+    featureString = str(featureCount) + " " +  str(time.strftime("%c")) \
+	 + " " + str(featureArgs) + " \n"
+    print("Complete Feature String: " + featureString)
+    featureFile.write(featureString)
+    featureFile.close()
+
+@bot.command()
+async def bug(*args):
+    """ Bug reports are held server side. Thanks for the help!
+
+        Syntax !bug (string)
+    """ 
+    bugCount = 0
+    bugFile = open('bugReport.txt', 'r+')
+    for i in bugFile.readlines():
+        bugCount += 1
+    for i in args:
+        bugArgs = " ".join(args)
+    bugString = str(bugCount) + " " +  str(time.strftime("%c")) \
+	 + " " + str(bugArgs) + " \n"
+    bugFile.write(featureString)
+    bugFile.close()
 
 # @bot.command()
 # async def google(args):
@@ -66,14 +112,26 @@ async def owpatch():
     await bot.say('Patch notes are too long for Discord, but you can find them here: ' + url)
 
 @bot.command()
-async def yt(args):
+async def source():
+    """ 
+    The bots source code can be found here.
+
+    Syntax: !source
+    """
+    url="https://www.github.com/luthes/dawgbot"
+    await bot.say('Source code on Github: ' + url)
+
+@bot.command()
+async def yt(*args):
     """ 
     Search YouTube for a video and disply the first result in chat
     
     Syntax: !yt overwatch video
     """
+    for i in args:
+        argstring = "+".join(args)
     yt = 'https://youtube.com'
-    r = requests.get(yt + '/results?search_query=' + args)
+    r = requests.get(yt + '/results?search_query=' + argstring)
     soup = BS(r.text, "html.parser")
     main_d = soup.find('div', id='results')
     items = main_d.find_all("div", class_='yt-lockup-content')
@@ -85,7 +143,7 @@ async def yt(args):
 @client.event
 async def on_member_join(member):
     """
-    Welcomes a new member to th channel.
+    Welcomes a new member to the channel.
 
     Syntax: none.
     """
